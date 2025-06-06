66e838207be2406f79d2363467a3696a15f380c3
ContraryXD <huynhhuuloc81206@gmail.com>
2025-06-06 13:02:25 +0700

    Refactor layout and breadcrumb components; add organizational chart image
    - Cleaned up formatting in RootLayout component for better readability.
    - Added a new image for the organizational chart.
    - Improved code structure and readability in BreadCrumb component, including consistent indentation and spacing.

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,13 +8,13 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }) {
-   return (
-      <html lang="en">
-         <body>
-            <Header />
-            <main>{children}</main>
-            <Footer />
-         </body>
-      </html>
-   );
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
